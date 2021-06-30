c1ab3ddf0ececf3992f9d13577fdf296087fdda8
Ivan Nasanovich <uolary@gmail.com>
2021-06-30 09:55:05 +0300

    Fix Cloud.js

--- server/Cloud.js
+++ server/Cloud.js
@@ -3,12 +3,6 @@ const constants = require('./constants');
 
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
-
 class Cloud {
   constructor() {
     cloudinary.config({
